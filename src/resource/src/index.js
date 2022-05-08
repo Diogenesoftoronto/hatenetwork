da43079b2c962daa7e7402c5c27139eff8a06f56
Diogenesoftoronto <87236699+Diogenesoftoronto@users.noreply.github.com>
2022-05-08 19:24:29 -0400

    Update index.js

--- src/resource/src/index.js
+++ src/resource/src/index.js
@@ -2,12 +2,12 @@ require('dotenv').config
 
 const express = require('express')
 const app = express()
-const port = 3000
+const port = process.env.RESOURCE_PORT || 4000
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
+  console.log(`Example resource server listening at http://localhost:${port}`)
 })
