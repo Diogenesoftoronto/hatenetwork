4dfa994ef13f92e7bcd6f93d51c9a3bf193577b2
Diogenesoftoronto <87236699+Diogenesoftoronto@users.noreply.github.com>
2022-05-08 19:24:41 -0400

    Update index.js

--- src/auth/src/index.js
+++ src/auth/src/index.js
@@ -2,12 +2,13 @@ require('dotenv').config
 
 const express = require('express')
 const app = express()
-const port = 3000
+const port = process.env.AUTH_PORT || 3000
+
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
+  console.log(`Example auth server listening at http://localhost:${port}`)
 })
